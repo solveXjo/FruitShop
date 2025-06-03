58f13224eaa93fe0a3a019ce3d1fa768fb7d8ffb
SolveX <fathii.alsadi@gmail.com>
2025-06-03 10:31:39 +0300

    make changes

---
+++ web/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6f80a2bd3523be869cb9d196618274629d09da96',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6f80a2bd3523be869cb9d196618274629d09da96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ip2location/ip2location-php' => array(
+            'pretty_version' => '9.7.3',
+            'version' => '9.7.3.0',
+            'reference' => '67c7bf13b0571b70925435e640ea5598e9fdeee9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ip2location/ip2location-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
